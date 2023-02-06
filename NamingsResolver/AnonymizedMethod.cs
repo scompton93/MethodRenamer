579bbc443866300bf68ceced62f57317728d72cd
Sean <scompton93@users.noreply.github.com>
2023-02-06 20:14:54 +1100

    Add project files.

---
+++ NamingsResolver/AnonymizedMethod.cs
@@ -0,0 +1,39 @@
+﻿using Mono.Cecil;
+using System.Text;
+
+namespace NamingsResolver;
+
+public class AnonymizedMethod
+{
+    public AnonymizedMethod(MethodDefinition md)
+    {
+        foreach (var parameter in md.Parameters)
+        {
+            parameters.Add(
+                parameter.ParameterType.Namespace != ""
+                    ? parameter.ParameterType.FullName
+                    : "Object"
+            );
+        }
+
+        ReturnType = md.ReturnType.Namespace != "" ? md.ReturnType.FullName : "Object";
+
+        OriginalDefinition = md.ToString();
+        DeclaringType = md.DeclaringType.Name;
+        MethodName = md.Name;
+        MethodFullName = md.FullName;
+    }
+
+    public string Definition =>
+        $"{ReturnType} {DeclaringType}::{MethodName}({string.Join(',', parameters)})";
+    public string AnonymousDefinition => $"{ReturnType} ({string.Join(',', parameters)})";
+
+    public string DeclaringType { get; private set; }
+    public string MethodName { get; private set; }
+    public string MethodFullName { get; private set; }
+
+    public string OriginalDefinition { get; private set; }
+
+    public string ReturnType { get; private set; }
+    public List<string> parameters { get; } = new List<string>();
+}
---
+++ NamingsResolver/Program.cs
@@ -0,0 +1,59 @@
+﻿using Mono.Cecil;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NamingsResolver;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        const string targetPath = @"C:\Users\x\Downloads\gamepack-deob.dll";
+        const string blueprintPath = @"C:\Users\x\Downloads\gamepack-deob - Copy.dll";
+        string outputPath = targetPath.Replace(".dll", "-renamed.dll");
+
+        var targetMethods = GetMethods(targetPath)
+            .GroupBy(o => o.AnonymousDefinition)
+            .Where(g => g.Count() == 1)
+            .SelectMany(g => g)
+            .Where(g => g.DeclaringType == "class35");
+        var blueprintMethods = GetMethods(blueprintPath)
+            .GroupBy(o => o.AnonymousDefinition)
+            .Where(g => g.Count() == 1)
+            .SelectMany(g => g);
+
+        var matchedMethods = targetMethods.Join(
+            blueprintMethods,
+            tagetMethod => tagetMethod.AnonymousDefinition,
+            blueprintMethod => blueprintMethod.AnonymousDefinition,
+            (tagetMethod, blueprintMethod) => new { tagetMethod, blueprintMethod }
+        );
+
+        var module = ModuleDefinition.ReadModule(targetPath);
+        var methodsToRename = from type in module.Types from method in type.Methods select method;
+
+        foreach (var matchedMethod in matchedMethods)
+        {
+            var methodToRename = methodsToRename
+                .Where(g => g.FullName == matchedMethod.tagetMethod.MethodFullName)
+                .First();
+            methodToRename.Name = matchedMethod.blueprintMethod.MethodName;
+            methodToRename.DeclaringType.Name = matchedMethod.blueprintMethod.DeclaringType;
+        }
+        module.Write(outputPath);
+    }
+
+    public static List<AnonymizedMethod> GetMethods(string target)
+    {
+        var module = ModuleDefinition.ReadModule(target);
+        var methods = from type in module.Types from method in type.Methods select method;
+
+        var anonymizedMethods = new List<AnonymizedMethod>();
+        foreach (var method in methods)
+        {
+            anonymizedMethods.Add(new AnonymizedMethod(method));
+        }
+        return anonymizedMethods;
+    }
+}
